b29732d1adbaa0a609fa382594849a3ef343507e
CraigStojsin <stojsin@outlook.com>
2015-04-06 15:05:19 -0400

    Initial Commit

---
+++ Day1BitmakerTests/Day1BitmakerTests.m
@@ -0,0 +1,40 @@
+//
+//  Day1BitmakerTests.m
+//  Day1BitmakerTests
+//
+//  Created by Craig Stojsin on 2015-04-06.
+//  Copyright (c) 2015 Codes By Craig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Day1BitmakerTests : XCTestCase
+
+@end
+
+@implementation Day1BitmakerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
